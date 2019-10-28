423554cf1c2833b9fbf2def61ae5f10498d4799c
haohanyuzmx <735268835@qq.com>
2019-10-28 22:35:08 +0800

    fist work

---
+++ work1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+func factorial(n int,ch chan int) {
+	var res = 1
+	for i := 1; i <= n; i++ {
+		res *= i
+	}
+	ch<-res
+}
+func main() {
+	ch := make(chan int)
+	go func() {
+		for i := 1; i <= 20; i++ {
+			factorial(i, ch)
+		}
+	}()
+	go func() {
+	for i := range ch {
+		fmt.Println(i)
+	}
+}()
+time.Sleep(2e9)
+}
