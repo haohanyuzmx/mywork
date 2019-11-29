627cfd69f3d6d79ce4bbc6681c4c1b7b08e2f8ae
haohanyuzmx <735268835@qq.com>
2019-11-29 12:50:41 +0800

    i can't get in cqupt

--- lv2.go
+++ lv2.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"io/ioutil"
+	"net/http"
+	"regexp"
 	"strconv"
-	"time"
 )
-
-func main() {
-	timeLayout := "2006-01-02 15:04:05"
-	var slice []int64
-HAHA:
-	var timestamp string
-	fmt.Scanf("%s", &timestamp)
-	if timestamp == "result" {
-		goto HEHE
-	} else {
-		c, _ := strconv.ParseInt(timestamp, 10, 64)
-		slice = append(slice, c)
-		goto HAHA
-	}
-HEHE:
-	for _, v := range slice {
-		datetime := time.Unix(v, 0).Format(timeLayout)
-		fmt.Println(datetime)
+import _ "github.com/go-sql-driver/mysql"
+func main()  {
+	enhen,_:=sql.Open("mysql","root:@tcp(localhost:3306)/assignment?charset=utf8")
+	_,_=enhen.Prepare("create table students (id int(1),name varchar(10))")
+	for i:=2019201303;i<=2019210346 ;i++  {
+		a,_:=http.Get("http://jwzx.cqupt.edu.cn/kebiao/kb_stu.php?xh="+strconv.Itoa(i))
+		b,_:=ioutil.ReadAll(a.Body)
+		re,_:=regexp.Compile("i....")
+		c:=string(b)
+		c=re.FindString(c)
+		c=c[10:]
+		_,_=enhen.Exec("insert into students values(i"+"'"+c+"')")
 	}
 }
